70b8f9a75a87d624dbe751fb7ccc21737663d235
ibrahim-089 <hi756145@gmail.com>
2024-08-25 22:49:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#menu');
+
+const sidebar = document.querySelector('.side-bar');
+
+menu.addEventListener('click', function(){
+    side-bar.classList.toggle('show-sidebar');
+});
